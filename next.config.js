667fac92d36d291a0aa2e669779fdd8536be4b47
Livia <liviawrk@gmail.com>
2023-11-02 16:58:18 -0300

    ci: remove image config from next

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true
-  },
   env: {
     API_URL: "https://653dabacf52310ee6a9a439a.mockapi.io/spider-verse",
     DOMAIN_ORIGIN: "http://localhost:3000",
